4935eb82c02a9fd874b552b534015483267dab49
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-02-07 17:57:16 +0300

    update formula

--- gitin.rb
+++ gitin.rb
@@ -1,8 +1,8 @@
 class Gitin < Formula
   desc "commit/branch/workdir explorer for git"
   homepage "https://github.com/isacikgoz/gitin"
-  url "https://github.com/isacikgoz/gitin/releases/download/v0.1.1/gitin_0.1.1_darwin_amd64.tar.gz"
-  sha256 "078a7ffdd2970d4e5b806f8f35c8698067b0fc7b8f4a7ba81d618df4c4e805fa"
+  url "https://github.com/isacikgoz/gitin/releases/download/v0.1.2/gitin_0.1.2_darwin_amd64.tar.gz"
+  sha256 "52a0e77095382c1147b7149371ec6911a1370795ee6641e58454d0a1b28ece84"
   depends_on "libgit2"
 
   def install
@@ -10,6 +10,6 @@ class Gitin < Formula
   end
 
   test do
-    assert_match "gitin version 0.1.1", shell_output("#{bin}/gitin --version", 2)
+    assert_match "gitin version 0.1.2", shell_output("#{bin}/gitin --version", 2)
   end
 end
